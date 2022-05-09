2d5fecf31a3e553f633e1931a5c2a9937fce863d
IMAKASH07 <sriakashbalaji2012@gmail.com>
2022-05-09 11:46:35 +0530

    apout

--- src/pages/AboutPage/subcomponents.tsx
+++ src/pages/AboutPage/subcomponents.tsx
@@ -2,11 +2,17 @@ import { Button, Card, Figure, FormControl, InputGroup } from "react-bootstrap";
 import styled from "styled-components";
 
 export const Title = styled.p`
-  margin-left: 43%;
+  margin-left: 45%;
   color: #0f1043;
   font-size: 40px;
-  font-weight: 550;
+  font-weight: 1000;
   font-family: "Montserrat", sans-serif;
+
+  @media (max-width: 650px) {
+    width: 100%;
+    height: 40px;
+    margin-left:30%;
+  }
 `;
 
 interface ParaProps {
@@ -16,10 +22,11 @@ interface ParaProps {
 }
 export const Para = styled.p<ParaProps>`
   font-family: "Montserrat", sans-serif;
-  font-weight: 530;
+  font-weight: 500;
   font-size: 20px;
-  margin-left: 70%;
-  width: 55%;
+  //margin-left: %;
+  width: 50%;
+  //height: 20%;
   margin-left: ${({ marginLeft }) => (marginLeft ? `${marginLeft}%` : "3%")};
   margin-top: ${({ marginTop }) => (marginTop ? `${marginTop}%` : "0%")};
   color: ${({ color }) => (color ? color : "#000000")};
@@ -30,6 +37,8 @@ export const Para = styled.p<ParaProps>`
     width: 100%;
   }
 `;
+
+
 
 export const FlexWrap = styled.div`
   justify-content: space-around;
@@ -43,129 +52,56 @@ export const FlexWrap = styled.div`
 
 export const AboutImage = styled(Figure.Image)`
   width: 40%;
+  height: 100%;
+  margin: 0 -2%;
+  @media (max-width: 600px) {
+    width: 50%;
+    height: 180px;
+    margin-left:20%;
+  }
+  
 `;
 
-export const CardContainer = styled(Card)`
-  width: 45%;
-  height: 100px;
-  right: 23em;
-  bottom: -50px;
-  border-radius: 10px;
-  position: absolute;
-  background-color: #3335cf;
-  @media (max-width: 600px) {
-    width: 90%;
-    height: auto;
-    left: 1em;
-    bottom: -34px;
-    border-radius: 10px;
-    margin: 0px auto;
-    position: absolute;
-    background-color: #3335cf;
-  }
-  @media (max-width: 900px) {
-    width: 70%;
-    height: 50px;
-    left: 5em;
-    bottom: -30px;
-    border-radius: 10px;
-    position: absolute;
-    background-color: #3335cf;
-  }
-`;
 
-export const WWraper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  @media (max-width: 600px) {
-    margin: 5% auto;
-    width: 100%;
-  }
-`;
 
 export const Container4 = styled.div`
   height: 100px;
   position: relative;
   background-color: #fcfcff;
-  @media (max-width: 600px) {
-    width: auto;
-    height: auto;
-    margin-bottom: 10px;
-  }
-  @media (max-width: 900px) {
-    width: auto;
-    height: auto;
-    margin-bottom: 10px;
+  @media (max-width: 580px) {
+    width: 100%;
+    height: 40px;
   }
 `;
 
-export const Titles = styled.div`
-  color: white;
-  margin-top: 23px;
-  padding: 15px;
-  font-weight: 500;
-  font-size: 17px;
-  @media (max-width: 600px) {
-    font-size: 12px;
-    margin-top: 12px;
-    padding: 15px;
-    font-weight: 500;
-  }
-  @media (max-width: 900px) {
-    font-size: 12px;
-    margin: 30px auto;
-    padding: 15px;
-    font-weight: 500;
-  }
-`;
-
-export const Input = styled(InputGroup)`
-  width: 57%;
-  height: 22px;
-  margin-top: 33px;
-
-  @media (max-width: 600px) {
-    width: 40%;
-    height: -20px;
-    margin-top: 15px;
-  }
-  @media (max-width: 900px) {
-    width: 50%;
-    height: -20px;
-    margin: 30px auto;
-  }
-`;
-
-export const Form = styled(FormControl)`
-  border-radius: 20px;
-  border: none;
-`;
-
-export const ButtonS = styled(Button)`
-  border-radius: 20px;
-  background-color: #ff7b00;
-  color: white;
-  margin-left: -38px;
-  border: none;
-`;
 
 export const PrivacyContainer = styled.div`
   background-color: #ebebf8;
   height: 300px;
-  @media (max-width: 600px) {
+  @media (max-width: 580px) {
     width: 100%;
     height: auto;
+    margin-bottom: 10px;
   }
+
   @media (max-width: 900px) {
     width: auto;
-    height: auto;
+    height: 300px;
   }
 `;
+
 export const FooterWrapper = styled.div`
   display: flex;
   justify-content: space-around;
   padding-top: 75px;
 `;
+
+export const Logo = styled.img`
+height: 40%;
+width:10%;
+margin-left:93%;
+`;
+
 export const Logo1 = styled.img`
   display: flex;
   width: 45%;
@@ -187,6 +123,7 @@ export const FinalFooter = styled.div`
   margin-top: 20px;
   justify-content: space-between;
   display: flex;
+   font-family: 'Montserrat', sans-serif; 
   flex-wrap: wrap;
   cursor: pointer;
   background: #ebebf8;
@@ -215,4 +152,94 @@ export const Links = styled.div`
   font-size: 13px;
 `;
 
-export const FinalWrapper = styled.div``;
+export const FinalWrapper = styled.div`
+`;
+
+export const PageWrapper = styled.div`
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  @media (max-width: 415px) {
+    width: 98%;
+  }
+`;
+
+
+
+export const FormCard = styled(Card)`
+  display: flex;
+  justify-content: space-around;
+  z-index: 1000;
+  margin-top: 3rem;
+  background-color: rgb(51, 53, 207);
+  width: 50%;
+  height: 80px;
+  @media (max-width: 415px) {
+    width: 85%;
+    margin-top: -3%;
+  }
+`;
+
+export const FormCardContent = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const FormCardWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+`;
+
+export const FormCardInput = styled.input`
+  margin-right: 4%;
+  margin-top: 3%;
+  height: 55%;
+  width: 40%;
+  border-radius: 20px;
+  border: none;
+  ::placeholder {
+   font-family: 'Montserrat', sans-serif; 
+    font-size: 12px;
+    font-weight: 550;
+  }
+  @media (max-width: 650px) {
+    width: 60%;
+    height: 50%;
+  }
+`;
+export const FormCardText = styled(Card.Body)`
+  font-weight: 700;
+   font-family: 'Montserrat', sans-serif; 
+  color: white;
+  margin: auto 0;
+  padding-top: 4%;
+  padding-left: 8%;
+  @media (max-width: 800px) {
+    font-size: 12px;
+  }
+`;
+
+export const FormCardButton = styled.button`
+  z-index: 1000;
+  margin-top: 3.6%;
+   font-family: 'Montserrat', sans-serif; 
+  margin-left: 79%;
+  text-align: center;
+  position: absolute;
+  height: 36%;
+  width: 11%;
+  border: none;
+  font-size: 12px;
+  border-radius: 20px;
+  background-color: rgb(255, 123, 0);
+  color: rgb(255, 242, 230);
+  @media (max-width: 800px) {
+    font-size: 10px;
+    margin-right: 7%;
+    margin-top: 5%;
+    height:20px;
+    width: 15%;
+  }
+`;
