de559cfd216840d4b3a6412f594e7de5610e076d
Your Name <you@example.com>
2022-05-09 09:56:15 +0530

    pR

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,21 +1,21 @@
+import { ReactElement } from "react";
 import { Switch, Route } from "react-router-dom";
 import ROUTES from "./const/routes";
 import ProtectedRoute from "./container/ProtectedRoute";
-
 import { TestPage, User, StreamSelection, Result } from "./pages";
 import AboutPage from "./pages/AboutPage";
 
-const Routes = () => {
+const Routes = (): ReactElement => {
   return (
     <Switch>
       <Route exact path={ROUTES.LOGIN} component={User} />
-      < Route
+      <ProtectedRoute
         path={ROUTES.STREAMSELECTION}
         component={StreamSelection}
       />
-      < Route path={ROUTES.TEST} component={TestPage} />
-      <Route path={ROUTES.RESULTCOPY} component={Result} />
-      <Route path={ROUTES.ABOUT} component={AboutPage} />
+      <ProtectedRoute path={ROUTES.TEST} component={TestPage} />
+      <ProtectedRoute path={ROUTES.RESULTCOPY} component={Result} />
+      <ProtectedRoute path={ROUTES.ABOUT} component={AboutPage} />
     </Switch>
   );
 };
